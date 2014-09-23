7bd9ad41ae339a79ba870b439e18c774156310e6
Arnaud DEBRAY <adebray@e3r7p12.42.fr>
2014-09-23 22:40:16 +0200

    save up

--- ft_objects.js
+++ ft_objects.js
@@ -1,19 +1,12 @@
 function ft_createdxf() {
 	return {
-		header: [],
+		header: {},
 		classes: [],
 		tables: [],
 		blocks: [],
 		entities: [],
 		objects: [],
 		thumbnailimage: []
-	}
-}
-
-function ft_createformat(key, value) {
-	return {
-		key: key,
-		value: value
 	}
 }
 
@@ -24,4 +17,17 @@ function ft_createline(x1, y1, x2, y2) {
 		x2: x2,
 		y2: y2
 	}
+}
+
+function ft_createcanvas(size_x, size_y) {
+	if (!size_x)
+		var size_x = 5000;
+	if (!size_y)
+		var size_y = 5000;
+	var canvas = document.createElement("canvas");
+
+	canvas.setAttribute("class", "c_canvas");
+	canvas.setAttribute("width", size_x);
+	canvas.setAttribute("height", size_y);
+	return canvas;
 }
