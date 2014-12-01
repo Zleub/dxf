4c6bd046e9e01ca145312318d6d415e9f352636f
Arnaud Debray <debray.arnaud@gmail.com>
2014-12-01 16:05:41 +0100

    auto-home push

--- dxf.js
+++ dxf.js
@@ -1,121 +1,121 @@
 function ft_nextchar(c) {
-		return String.fromCharCode(c.charCodeAt(0) + 1);
-	}
+	return String.fromCharCode(c.charCodeAt(0) + 1);
+}
 
 function ft_toarray() {
-		var pattern = /\r\n|\r|\n */g;
-		var array = this.str.split(pattern);
+	var pattern = /\r\n|\r|\n */g;
+	var array = this.str.split(pattern);
 
-		for (var i = 0; i < array.length; i++)
-			array[i] = array[i].trim();
-		this.array = array;
-	};
+	for (var i = 0; i < array.length; i++)
+		array[i] = array[i].trim();
+	this.array = array;
+}
 
 function ft_getduo() {
-		var tmp = {}
+	var tmp = {}
 
-		tmp[0] = this.array[this.index];
-		tmp[1] = this.array[this.index + 1];
-		this.index += 2;
-		return tmp;
-	};
+	tmp[0] = this.array[this.index];
+	tmp[1] = this.array[this.index + 1];
+	this.index += 2;
+	return tmp;
+}
 
 function ft_getsection() {
-		this.current = this.ft_getduo();
-		if (this.current[0] == '2' && this.current[1] == 'HEADER')
-			this.section = this.current[1];
-		else if (this.current[0] == '2' && this.current[1] == 'CLASSES')
-			this.section = this.current[1];
-		else if (this.current[0] == '2' && this.current[1] == 'TABLES')
-			this.section = this.current[1];
-		else if (this.current[0] == '2' && this.current[1] == 'BLOCKS')
-			this.section = this.current[1];
-		else if (this.current[0] == '2' && this.current[1] == 'ENTITIES')
-			this.section = this.current[1];
-		else if (this.current[0] == '2' && this.current[1] == 'OBJECTS')
-			this.section = this.current[1];
-		else if (this.current[0] == '2' && this.current[1] == 'ACDSDATA')
-			this.section = this.current[1];
-		this.current = this.ft_getduo();
-	};
+	this.current = this.ft_getduo();
+	if (this.current[0] == '2' && this.current[1] == 'HEADER')
+		this.section = this.current[1];
+	else if (this.current[0] == '2' && this.current[1] == 'CLASSES')
+		this.section = this.current[1];
+	else if (this.current[0] == '2' && this.current[1] == 'TABLES')
+		this.section = this.current[1];
+	else if (this.current[0] == '2' && this.current[1] == 'BLOCKS')
+		this.section = this.current[1];
+	else if (this.current[0] == '2' && this.current[1] == 'ENTITIES')
+		this.section = this.current[1];
+	else if (this.current[0] == '2' && this.current[1] == 'OBJECTS')
+		this.section = this.current[1];
+	else if (this.current[0] == '2' && this.current[1] == 'ACDSDATA')
+		this.section = this.current[1];
+	this.current = this.ft_getduo();
+}
 
 function ft_parse_header() {
-		var tmp = this.current;
+	var tmp = this.current;
 
-		this.current = this.ft_getduo();
-		this.dt_header[tmp[1]] = this.current[1];
-	};
+	this.current = this.ft_getduo();
+	this.dt_header[tmp[1]] = this.current[1];
+}
 
 function ft_parse_class () {
-		var index = this.dt_classes.length;
+	var index = this.dt_classes.length;
 
-		this.dt_classes.push({});
+	this.dt_classes.push({});
+	this.current = this.ft_getduo();
+	while (this.current[0] != '0')
+	{
+		this.dt_classes[index][this.current[0]] = this.current[1];
 		this.current = this.ft_getduo();
-		while (this.current[0] != '0')
-		{
-			this.dt_classes[index][this.current[0]] = this.current[1];
-			this.current = this.ft_getduo();
-		}
-	};
+	}
+}
 
 function ft_parse_table() {
-		var index = this.dt_tables.length;
+	var index = this.dt_tables.length;
 
-		this.dt_tables.push({});
-		this.current = this.ft_getduo();
-		while (this.current[1] != 'ENDTAB')
+	this.dt_tables.push({});
+	this.current = this.ft_getduo();
+	while (this.current[1] != 'ENDTAB')
+	{
+		var tmp;
+		if (this.dt_tables[index]['name'] == undefined && this.current[0] == '2')
 		{
-			var tmp;
-			if (this.dt_tables[index]['name'] == undefined && this.current[0] == '2')
+			tmp = 'A';
+			this.dt_tables[index]['name'] = this.current[1];
+			while (this.current[0] != '0')
 			{
-				tmp = 'A';
-				this.dt_tables[index]['name'] = this.current[1];
-				while (this.current[0] != '0')
-				{
-					this.dt_tables[index][this.current[0]] = this.current[1];
-					this.current = this.ft_getduo();
-				}
-			}
-			else if (this.current[1] == this.dt_tables[index]['name'])
-			{
-				this.dt_tables[index][tmp] = {};
+				this.dt_tables[index][this.current[0]] = this.current[1];
 				this.current = this.ft_getduo();
-
-				while (this.current[0] != '0')
-				{
-					this.dt_tables[index][tmp][this.current[0]] = this.current[1];
-					this.current = this.ft_getduo();
-				}
-				tmp = this.ft_nextchar(tmp);
 			}
 		}
-		this.current = this.ft_getduo();
+		else if (this.current[1] == this.dt_tables[index]['name'])
+		{
+			this.dt_tables[index][tmp] = {};
+			this.current = this.ft_getduo();
+
+			while (this.current[0] != '0')
+			{
+				this.dt_tables[index][tmp][this.current[0]] = this.current[1];
+				this.current = this.ft_getduo();
+			}
+			tmp = this.ft_nextchar(tmp);
+		}
 	}
+	this.current = this.ft_getduo();
+}
 
 function ft_parse_block() {
-		var index = this.dt_blocks.length;
+	var index = this.dt_blocks.length;
 
-		this.dt_blocks.push({});
+	this.dt_blocks.push({});
+	this.current = this.ft_getduo();
+	while (this.current[0] != '0')
+	{
+		this.dt_blocks[index][this.current[0]] = this.current[1];
 		this.current = this.ft_getduo();
-		while (this.current[0] != '0')
-		{
-			this.dt_blocks[index][this.current[0]] = this.current[1];
-			this.current = this.ft_getduo();
-		}
 	}
+}
 
 function ft_parse_entity() {
-		var index = this.dt_entities.length;
+	var index = this.dt_entities.length;
 
-		this.dt_entities.push({});
+	this.dt_entities.push({});
+	this.dt_entities[index][this.current[0]] = this.current[1];
+	this.current = this.ft_getduo();
+	while (this.current[0] != '0')
+	{
 		this.dt_entities[index][this.current[0]] = this.current[1];
 		this.current = this.ft_getduo();
-		while (this.current[0] != '0')
-		{
-			this.dt_entities[index][this.current[0]] = this.current[1];
-			this.current = this.ft_getduo();
-		}
 	}
+}
 
 function ft_upper(arg, val) {
 	if (typeof arg == 'undefined' && typeof val != null)
@@ -134,68 +134,68 @@ function ft_lower(arg, val) {
 }
 
 function ft_seeksize(entity, index, array) {
-		if (entity == null)
-			return ;
-		else if (entity[0] != 'LINE' && entity[0] != 'MTEXT')
-			return ;
+	if (entity == null)
+		return ;
+	else if (entity[0] != 'LINE' && entity[0] != 'MTEXT' && entity[0] != 'TEXT')
+		return ;
 
-		arguments.callee.x_min = ft_upper(arguments.callee.x_min, entity[10])
-		arguments.callee.x_max = ft_lower(arguments.callee.x_max, entity[10])
-		arguments.callee.y_min = ft_upper(arguments.callee.y_min, entity[20])
-		arguments.callee.y_max = ft_lower(arguments.callee.y_max, entity[20])
+	arguments.callee.x_min = ft_upper(arguments.callee.x_min, entity[10])
+	arguments.callee.x_max = ft_lower(arguments.callee.x_max, entity[10])
+	arguments.callee.y_min = ft_upper(arguments.callee.y_min, entity[20])
+	arguments.callee.y_max = ft_lower(arguments.callee.y_max, entity[20])
 
-		if (entity[0] == 'LINE')
-		{
-			arguments.callee.x_min = ft_upper(arguments.callee.x_min, entity[11])
-			arguments.callee.x_max = ft_lower(arguments.callee.x_max, entity[11])
-			arguments.callee.y_min = ft_upper(arguments.callee.y_min, entity[21])
-			arguments.callee.y_max = ft_lower(arguments.callee.y_max, entity[21])
-		}
+	if (entity[0] == 'LINE')
+	{
+		arguments.callee.x_min = ft_upper(arguments.callee.x_min, entity[11])
+		arguments.callee.x_max = ft_lower(arguments.callee.x_max, entity[11])
+		arguments.callee.y_min = ft_upper(arguments.callee.y_min, entity[21])
+		arguments.callee.y_max = ft_lower(arguments.callee.y_max, entity[21])
 	}
+}
 
 function ft_parse() {
-		while (this.index < this.array.length - 2)
-		{
-			if (this.current[0] == '0' && this.current[1] == 'SECTION')
-				this.ft_getsection();
-			else if (this.current[0] == '0' && this.current[1] == 'ENDSEC')
-				this.current = this.ft_getduo();
-			else if (this.current[0] == '9' && this.section == 'HEADER')
-				this.ft_parse_header();
-			else if (this.current[0] == '0' && this.section == 'CLASSES')
-				this.ft_parse_class();
-			else if (this.current[0] == '0' && this.section == 'TABLES')
-				this.ft_parse_table();
-			else if (this.current[0] == '0' && this.section == 'BLOCKS')
-				this.ft_parse_block();
-			else if (this.current[0] == '0' && this.section == 'ENTITIES')
-				this.ft_parse_entity();
-			else
-				this.current = this.ft_getduo();
-		}
+	while (this.index < this.array.length - 2)
+	{
+		if (this.current[0] == '0' && this.current[1] == 'SECTION')
+			this.ft_getsection();
+		else if (this.current[0] == '0' && this.current[1] == 'ENDSEC')
+			this.current = this.ft_getduo();
+		else if (this.current[0] == '9' && this.section == 'HEADER')
+			this.ft_parse_header();
+		else if (this.current[0] == '0' && this.section == 'CLASSES')
+			this.ft_parse_class();
+		else if (this.current[0] == '0' && this.section == 'TABLES')
+			this.ft_parse_table();
+		else if (this.current[0] == '0' && this.section == 'BLOCKS')
+			this.ft_parse_block();
+		else if (this.current[0] == '0' && this.section == 'ENTITIES')
+			this.ft_parse_entity();
+		else
+			this.current = this.ft_getduo();
+	}
 
-		this.dt_entities.forEach(this.ft_seeksize);
+	this.dt_entities.forEach(this.ft_seeksize);
 
-		this.width = this.ft_seeksize.x_max - this.ft_seeksize.x_min;
-		this.height = this.ft_seeksize.y_max - this.ft_seeksize.y_min;
-	};
+	this.width = this.ft_seeksize.x_max - this.ft_seeksize.x_min;
+	this.height = this.ft_seeksize.y_max - this.ft_seeksize.y_min;
+}
 
 function ft_get_group(group_name) {
-		if (!this.kt_groups[group_name])
-			this.kt_groups[group_name] = new Kinetic.Group(
-				{
-					name: group_name,
-				});
-		return this.kt_groups[group_name]
-	};
+	if (!this.kt_groups[group_name])
+		this.kt_groups[group_name] = new Kinetic.Group(
+		{
+			name: group_name,
+		});
+	return this.kt_groups[group_name]
+}
 
 function ft_shape_line(entity, index) {
-		var x_min = this.ft_seeksize.x_min;
-		var y_min = this.ft_seeksize.y_min;
+	var x_min = this.ft_seeksize.x_min;
+	var y_min = this.ft_seeksize.y_min;
 
-		if (entity[8])
-			var group = this.ft_get_group(entity[8]);
-		this.kt_shapes[index] = new Kinetic.Shape({
+	if (entity[8])
+		var group = this.ft_get_group(entity[8]);
+	this.kt_shapes[index] = new Kinetic.Shape({
 		name: entity[5],
 		x: 0,
 		y: 0,
@@ -211,16 +211,16 @@ function ft_shape_line(entity, index) {
 				);
 			context.stroke();
 		}
-		});
-		group.add(this.kt_shapes[index]);
-	};
+	});
+	group.add(this.kt_shapes[index]);
+}
 
-function ft_getdot(str){
+function ft_getdot(str) {
 	var i = 0;
 	var len = str.length
 
 	while (str[i] && str[i] != ';')
-		i += 1;
+	i += 1;
 	if (i == len)
 		return 0;
 	else
@@ -236,30 +236,30 @@ function ft_regexion(str) {
 }
 
 function ft_shape_text(entity, index) {
-		var x_min = this.ft_seeksize.x_min;
-		var y_min = this.ft_seeksize.y_min;
+	var x_min = this.ft_seeksize.x_min;
+	var y_min = this.ft_seeksize.y_min;
 
-		if (entity[8])
-			var group = this.ft_get_group(entity[8]);
-		this.kt_shapes[index] = new Kinetic.Shape({
-			name: entity[5],
-			x: 0,
-			y: 0,
-			fill: 'red',
-			drawFunc: function(context) {
-				context.fillText(
+	if (entity[8])
+		var group = this.ft_get_group(entity[8]);
+	this.kt_shapes[index] = new Kinetic.Shape({
+		name: entity[5],
+		x: 0,
+		y: 0,
+		fill: 'red',
+		drawFunc: function(context) {
+			context.fillText(
+				// entity[1],
 					ft_regexion(entity[1]).replace(/\}/, "").replace(/\\./g, " "),
 					parseInt(entity[10]) - x_min + 100,
 					parseInt(entity[20]) - y_min + 100
-				);
-			}
-		});
-		print(group)
-		group.add(this.kt_shapes[index]);
-	};
+					);
+		}
+	});
+	print(group)
+	group.add(this.kt_shapes[index]);
+}
 
-function ft_toJPEG()
-{
+function ft_toJPEG() {
 	return this.kt_layer.toImage({
 		mimeType : "image/png",
 		x:0,
@@ -267,40 +267,42 @@ function ft_toJPEG()
 		width: this.width + 2,
 		height: this.height + 2,
 		callback: function(img) {
-				MyImage = img
+			MyImage = img
 		}
 	});
-};
+}
 
 function ft_toKinetic(bool) {
-		if (document.getElementById('container'))
-		{
-			if (bool == 'stage')
-				this.kt_stage = new Kinetic.Stage({
-					container: 'container',
-					width: this.width + 200,
-					height: this.height + 200
-				});
-			this.kt_layer = new Kinetic.Layer({});
-			this.kt_groups = [];
-			this.kt_shapes = [];
-			for (var i = 0; i < this.dt_entities.length; i++) { // ENTITY SELECTION TO SHAPE
-				if (this.dt_entities[i][0] == 'LINE')
-					this.ft_shape_line(this.dt_entities[i], i);
-				else if (this.dt_entities[i][0] == 'MTEXT')
-					this.ft_shape_text(this.dt_entities[i], i);
-			};
-			for (i in this.kt_groups) {
-				this.kt_layer.add(this.kt_groups[i]);
-			};
-			if (bool == 'stage')
-				this.kt_stage.add(this.kt_layer)
+	if (document.getElementById('container'))
+	{
+		if (bool == 'stage')
+			this.kt_stage = new Kinetic.Stage({
+				container: 'container',
+				width: this.width + 200,
+				height: this.height + 200
+			});
+		this.kt_layer = new Kinetic.Layer({});
+		this.kt_groups = [];
+		this.kt_shapes = [];
+		for (var i = 0; i < this.dt_entities.length; i++) { // ENTITY SELECTION TO SHAPE
+			if (this.dt_entities[i][0] == 'LINE')
+				this.ft_shape_line(this.dt_entities[i], i);
+			else if (this.dt_entities[i][0] == 'MTEXT')
+				this.ft_shape_text(this.dt_entities[i], i);
+			else if (this.dt_entities[i][0] == 'TEXT')
+				this.ft_shape_text(this.dt_entities[i], i);
+		};
+		for (i in this.kt_groups) {
+			this.kt_layer.add(this.kt_groups[i]);
+		};
+		if (bool == 'stage')
+			this.kt_stage.add(this.kt_layer)
 
-			this.ft_toJPEG()
-		}
-		else
-			log('provide container plz')
-};
+		this.ft_toJPEG()
+	}
+	else
+		log('provide container plz')
+}
 
 function DXF(BinaryString, bool) {
 	this.str = BinaryString;
@@ -333,6 +335,13 @@ function DXF(BinaryString, bool) {
 	this.ft_shape_text = ft_shape_text;
 	this.ft_toKinetic = ft_toKinetic;
 	this.ft_toJPEG = ft_toJPEG;
+
+	this.ft_print = function () {
+		for (var i = 0; i < this.dt_entities.length; i++) {
+			if (this.dt_entities[i][0] == 'MTEXT' || this.dt_entities[i][0] == 'TEXT')
+				print(i + "  " + this.dt_entities[i][0] + "  " + this.dt_entities[i][1])
+		};
+	}
 
 	// CONSTRUCTOR
 	this.ft_toarray();
